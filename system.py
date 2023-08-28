d47f8089097c316711c8a7733c7def132b6eb884
kaga kusha <143403961+kaga-kuha@users.noreply.github.com>
2023-08-28 14:49:05 +0300

    Add files via upload

---
+++ system.py
@@ -0,0 +1,143 @@
+import os
+print("""
+                        .:iiri:..                               
+                    rEBBBBQBQBQBQQEu.                               
+                  .BBZrvgZbqPqPqdEgQB1                              
+                  jBK    MqqXqXqXKXbZB                    
+                  1BQ.  YRZbbXqSKSKKEQ                
+                  7BRBBBBgZZPPXKXKXqbB                              
+                            :DqXKXKXdQ                
+           i2PKPKKI1j1u1u1Js2dXKXqSKPQ   i:::.                      
+         .RBQgMggDMggDgDgDgEbKqXKXKXdQ  vBBQBBM:                    
+         BQbqPXPKqKPKqXqKPKqXqSqXqXqdB  iBPPqdMBr                   
+        1BPXqXKXqSqXqqPKqqPqPKPKPqbdBD  iQPXKXPZB.                  
+        ggKqXqXqXPqEDQRQQQQQQQQBQBBBX   7QqKXqXPBJ                  
+        BdqXKXqKPPQQqY777r7r7rrii:.     RbPSqXKKQ2                  
+        QEXKSKSqPQL                   rRZPXKXqXPgX                  
+        QdPSKXKqQ7    i7sJI2XSX5KSKXEQQdqKqSqXKqQ2                  
+        ERqKXKSdg   sBBQRQgRggggDgggZEqqSqSKSqXbB7                  
+        iBZqqKPdg  iBZbqPKPKPXPKPKPXPKKSKSKSKKPgB                   
+         2QQEEbgQ  uQPXqXKXKXPKqKPKPKPKPqqKPqPDBr                   
+          ibQQBQB  vQKKXqSqKPgQRQRQRQRQQQQQQBBBr                    
+                   sQPXqXqKPq::::i:i:ii77Lvsvi                      
+                   LQqqXqXqPq                                       
+                   sQPSKSKXPPMQQBBQBBBB.                            
+                   JQPKXKSKKqKPZb   iBB.                            
+                   UQEqPXqKqSqXZ5   .BB.                            
+                    PBBMgEEPPPbdQQPDBBU                             
+                     .7SbQRBQBQBQBMSi. 
+=========================================================
+        fills[1]             club[2]        the-places[3]
+                                                         
+      mk-fill[4]                 [5]                  [6]
+=========================================================
+""")
+input1 = input("echo:")
+
+if input1 == '1':
+    print("=====================echo-neme-fill======================")
+    print("sinon.chr","meebl.chr",sep='    ')
+    print("=========================================================")
+    input2 = input("name -fill:")
+    if input2 == 'sinon.chr':
+        print("=========================================================")
+        print("   delet-fill[1] exit-no-the-game[2]              info[3]")
+        print("                                                         ")
+        print("             [4]                 [5]                  [6]")
+        print("=========================================================")
+        input3 = input("")
+        if input3 == '1':
+            os.remove("Dignitaries/sinon.chr")
+        if input3 == '2':
+            print("exit-[sinon]")
+        if input3 == '3':
+            print("┌───────────┬───────────────────┬─────────┬────────┬─────────────────────────────┬──────┬────────┬──────────┐")
+            print("│    IP▴    │    domain-name    │  Name:  │  game: │         Vendor              │ Sent │ Recvd  │   Seen   │")
+            print("├───────────┼───────────────────┼─────────┼────────┼─────────────────────────────┼──────┼────────┼──────────┤")
+            print("│ xx.x.x.xx │ xx:xx:xx:xx:xx:xx │  sinon  │  xxxx  │  PCS Computer Systems GmbH  │ x B  │ x B    │ xx:xx:xx │")
+            print("└───────────┴───────────────────┴─────────┴────────┴─────────────────────────────┴──────┴────────┴──────────┘")
+            # input()
+
+    if input2 == 'meebl.chr':
+        print("=========================================================")
+        print("   delet-fill[1] exit-no-the-game[2]              info[3]")
+        print("                                                         ")
+        print("             [4]                 [5]                  [6]")
+        print("=========================================================")
+        input3 = input("")
+        if input3 == '1':
+            os.remove("Dignitaries/meebl.chr")
+        if input3 == '2':
+            print("exit-[meebl]")
+        if input3 == '3':
+            print("┌───────────┬───────────────────┬─────────┬────────┬─────────────────────────────┬──────┬────────┬──────────┐")
+            print("│    IP▴    │    domain-name    │  Name:  │  game: │         Vendor              │ Sent │ Recvd  │   Seen   │")
+            print("├───────────┼───────────────────┼─────────┼────────┼─────────────────────────────┼──────┼────────┼──────────┤")
+            print("│ xx.x.x.xx │ xx:xx:xx:xx:xx:xx │  meebl  │  xxxx  │  PCS Computer Systems GmbH  │ x B  │ x B    │ xx:xx:xx │")
+            print("└───────────┴───────────────────┴─────────┴────────┴─────────────────────────────┴──────┴────────┴──────────┘")
+            # input()
+if input1 == '2':
+    # exit()
+    print("=========================================================")
+    print("        fills[1]             text[2]        the-places[3]")
+    print("                                                         ")
+    print("      mk-fill[4]                 [5]                  [6]")
+    print("=========================================================")
+    input2 = input("echo:")
+    if input2 == '1':
+        print(1000*"satori.chr","moniko.chr","yuki.chr","naruki.chr","femk.chr", sep='    ')
+        input3 = input("Choose-a-file:")
+        if input3 == 'satori.chr':
+                    print("=========================================================")
+                    print("   delet-fill[1]                 [2]              info[3]")
+                    print("                                                         ")
+                    print("             [4]                 [5]                  [6]")
+                    print("=========================================================")
+                    input2 = input('echo:')
+                    if input2 == "3":
+                                    print("┌────────────┬────────────┐")
+                                    print("│ name:      │   satori   │")
+                                    print("├────────────┼────────────┤")
+                                    print("│ club:      │            │")
+                                    print("├────────────┼────────────┤")
+                                    print("│            │            │")                        
+                                    print("├────────────┼────────────┤")
+                                    print("│            │            │")                        
+                                    print("└────────────┴────────────┘")
+
+    if input2 == '2':
+        text1 = input("texit-1:")
+        text2 = input("texit-2:")
+        text3 = input("texit-3:")        
+        text4 = input("texit-4:")
+        print("=========================================================")
+        print("       satori[1]           moniko[2]              yuki[3]")
+        print("                                                         ")
+        print("       naruki[4]             femk[5]                  [6]")
+        print("=========================================================")
+        input3 = input("speaker:")
+        if input3 == '1':
+            print('satori-1:',text1)
+            print('satori-2:',text2)
+            print('satori-3:',text3)
+            print('satori-4:',text4)
+        if input3 == '2':
+            print('moniko-1:',text1)
+            print('moniko-2:',text2)
+            print('moniko-3:',text3)
+            print('moniko-4:',text4)
+        if input3 == '3':
+            print('yuki-1:',text1)
+            print('yuki-2:',text2)
+            print('yuki-3:',text3)
+            print('yuki-4:',text4)
+        if input3 == '4':
+            print('naruki-1:',text1)
+            print('naruki-2:',text2)
+            print('naruki-3:',text3)
+            print('naruki-4:',text4)
+        if input3 == '5':
+            print('femk-1:',text1)
+            print('femk-2:',text2)
+            print('femk-3:',text3)
+            print('femk-4:',text4)
